0f3661fb83c6392a75daa978aa12fff1600d1fcb
Shaik Karimulla <karimulla.shaik@byjus.com>
2022-07-29 11:20:04 +0530

    feat: added header in client side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+import "./App.css";
+import logo from "./assets/react.svg";
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+const App = () => {
+    return (
+        <div class="container-fluid p-0">
+            <header className="App-header navbar navbar-expand navbar-dark flex-column flex-md-row bd-navbar">
+                <img src={logo} className="App-logo" alt="logo" height="40px" />
+                React Electricity App
+            </header>
+        </div>
+    );
+};
 
-export default App
+export default App;
